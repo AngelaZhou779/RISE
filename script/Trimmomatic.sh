b7f243e9eac4b1850f1842cf002088a9b60f720c
AngelaZhou779 <78465068+AngelaZhou779@users.noreply.github.com>
2021-09-28 14:53:20 -0400

    Create Trimmomatic.sh

---
+++ script/Trimmomatic.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=EC-12_R1_001.fq --output=z01.%x
+#SBATCH --mail-type=END,FAIL --mail-user=zz220@georgetown.edu
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1 --time=72:00:00
+#SBATCH --mem=4G
+
+# --------------------------------------------------------------------#
+# This script runs trimmomatic, output file go in working directory  #
+#---------------------------------------------------------------------#
+
+#- DEFINE FILE LOCATIONS--------------------------------------------#
+
+trim_prog=/home/zz220/trimmomatic/Trimmomatic-0.39/trimmomatic-0.39.jar
+
+input=/home/zz220/RISE/M1_S4_L001_R1_001.fastq.gz
+
+output=/home/zz220/RISE/M1_S4_L001_R1_001.cln.fastq.gz
+
+#- RUN command ----------------#
+java -jar trimmomatic-0.39.jar SE \
+$input \
+$output \
+ILLUMINACLIP:smRNA_NexFlex_adapters.fa:2:30:10
+HEADCROP:10 \
+TRAILING:10 \
+SLIDINGWINDOW:4:15 \
+MINLEN:17
+#- FIN -----------------------#
